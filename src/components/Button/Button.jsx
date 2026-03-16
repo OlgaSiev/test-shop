2783356c54d5fdb1d24c1dad9bb6c3a1f1b33804
Olha Sievierina <olgasievierina@ukr.net>
2026-03-16 14:06:52 +0200

    Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cn from "classnames";
 import PropTypes from "prop-types";
-import {Link} from "react-router";
+import {Link} from "react-router-dom";
 
 import "./Button.scss";
 
